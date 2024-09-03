2d54eb311caa9ddd8e2e07aa56fa6aa84a1b4594
sathishkumar <satish102719@gmail.com>
2024-09-03 14:23:57 +0530

    Add the Discount Proce

--- app/src/main/java/com/example/shoppinggroceryapp/fragments/retailerfragments/inventoryfragments/AddEditFragment.kt
+++ app/src/main/java/com/example/shoppinggroceryapp/fragments/retailerfragments/inventoryfragments/AddEditFragment.kt
@@ -8,6 +8,8 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Im
 import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -21,12 +23,16 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.drawToBitmap
 import androidx.core.view.setPadding
+import androidx.core.widget.addTextChangedListener
 import com.example.shoppinggroceryapp.R
 import com.example.shoppinggroceryapp.fragments.ImageHandler
 import com.example.shoppinggroceryapp.fragments.appfragments.InitialFragment
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.textfield.TextInputEditText
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class AddEditFragment : Fragment() {
 
@@ -47,21 +53,113 @@ class AddEditFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_add_edit, container, false)
         val updateBtn = view.findViewById<MaterialButton>(R.id.updateInventoryBtn)
+        var offer = 0f
+        var price = 0f
         val productName = view.findViewById<TextInputEditText>(R.id.productNameEditFrag)
         val brandName = view.findViewById<TextInputEditText>(R.id.productBrandEditFrag)
-        val productParentCategory = view.findViewById<TextInputEditText>(R.id.productParentCatLayout)
+        val productParentCategory = view.findViewById<TextInputEditText>(R.id.productParentCatEditFrag)
         val productSubCat = view.findViewById<TextInputEditText>(R.id.productCategoryEditFrag)
         val productDescription = view.findViewById<TextInputEditText>(R.id.productDescriptionEditFrag)
         val productPrice = view.findViewById<TextInputEditText>(R.id.productPriceEditFrag)
         val productOffer = view.findViewById<TextInputEditText>(R.id.productOfferEditFrag)
         val productQuantity = view.findViewById<TextInputEditText>(R.id.productQuantityEditFrag)
-        val productAvailableItems = view.findViewById<TextInputEditText>(R.id.productManufactureEditFrag)
+        val discount = view.findViewById<TextInputEditText>(R.id.productDiscountEditFrag)
+        val productAvailableItems = view.findViewById<TextInputEditText>(R.id.productAvailableItemsEditFrag)
         val isVeg = view.findViewById<CheckBox>(R.id.productIsVegEditFrag)
-        val productManufactureDate = view.findViewById<TextInputEditText>(R.id.productAvailableItemsEditFrag)
+        val productManufactureDate = view.findViewById<TextInputEditText>(R.id.productManufactureEditFrag)
         val productExpiryDate = view.findViewById<TextInputEditText>(R.id.productExpiryEditFrag)
 
-        updateBtn.setOnClickListener {
+        val formatter = SimpleDateFormat("yyyy-MM-dd",Locale.getDefault())
 
+
+        val dateManufacturePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select the Birthday Date")
+            .setTextInputFormat(SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()))
+            .setInputMode(MaterialDatePicker.INPUT_MODE_CALENDAR)
+            .build()
+
+        val dateExpiryPicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select the Birthday Date")
+            .setTextInputFormat(SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()))
+            .setInputMode(MaterialDatePicker.INPUT_MODE_CALENDAR)
+            .build()
+
+        dateManufacturePicker.addOnPositiveButtonClickListener {
+            productManufactureDate.setText(formatter.format(it))
+        }
+        dateExpiryPicker.addOnPositiveButtonClickListener {
+            productExpiryDate.setText(formatter.format(it))
+        }
+        productManufactureDate.setOnClickListener {
+            dateManufacturePicker.show(parentFragmentManager,"Manufacture Date")
+        }
+        productExpiryDate.setOnClickListener {
+            dateExpiryPicker.show(parentFragmentManager,"Expiry Date")
+        }
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                println(s)
+                if(s?.isNotEmpty()==true) {
+                    val priceInt = s.toString().toFloat()
+                    val discountedPrice = (priceInt) - ((offer / 100) * priceInt)
+                    price = discountedPrice
+                    println(discountedPrice)
+                    discount.setText(discountedPrice.toString())
+                }
+            }
+        }
+        val offerTextWatcher = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if(s?.isNotEmpty()==true) {
+                    val offerInt = s.toString().toFloat()
+                    if (offerInt in 0f..100f) {
+                        offer = offerInt
+                    }
+                    val discountedPrice = (price) - ((offer / 100) * price)
+                    println("Price: ${discountedPrice}")
+                    discount.setText(discountedPrice.toString())
+                }
+                else{
+                    discount.setText(price.toString())
+                }
+            }
+
+        }
+        productPrice.addTextChangedListener(textWatcher)
+        productOffer.addTextChangedListener(offerTextWatcher)
+        updateBtn.setOnClickListener {
+            if(productName.text.toString().isNotEmpty()&&
+                productDescription.text.toString().isNotEmpty()&&
+                productSubCat.text.toString().isNotEmpty()&&
+                brandName.text.toString().isNotEmpty()&&
+                productParentCategory.text.toString().isNotEmpty()&&
+                productPrice.text.toString().isNotEmpty()&&
+                productOffer.text.toString().isNotEmpty()&&
+                productQuantity.text.toString().isNotEmpty()&&
+                productAvailableItems.text.toString().isNotEmpty()&&
+                productManufactureDate.text.toString().isNotEmpty()&&
+                productExpiryDate.text.toString().isNotEmpty()){
+                Toast.makeText(context,"Updated Successfully",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(context,"All the Fields are mandatory",Toast.LENGTH_SHORT).show()
+            }
         }
         view.findViewById<ImageView>(R.id.addNewImage).setOnClickListener {
             imageHandler.showAlertDialog()
