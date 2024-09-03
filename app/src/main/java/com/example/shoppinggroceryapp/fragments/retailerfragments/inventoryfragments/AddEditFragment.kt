56cb8f6515c11171430f1fa33cf98fdca8cd1f0d
sathishkumar <satish102719@gmail.com>
2024-09-03 13:04:41 +0530

    Get the image from user

--- app/src/main/java/com/example/shoppinggroceryapp/fragments/retailerfragments/inventoryfragments/AddEditFragment.kt
+++ app/src/main/java/com/example/shoppinggroceryapp/fragments/retailerfragments/inventoryfragments/AddEditFragment.kt
@@ -12,22 +12,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.HorizontalScrollView
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.drawToBitmap
 import androidx.core.view.setPadding
 import com.example.shoppinggroceryapp.R
 import com.example.shoppinggroceryapp.fragments.ImageHandler
+import com.example.shoppinggroceryapp.fragments.appfragments.InitialFragment
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class AddEditFragment : Fragment() {
 
 
     private lateinit var imageHandler: ImageHandler
     var count = 0
+    var imageList = mutableListOf<Bitmap>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         imageHandler = ImageHandler(this)
@@ -40,20 +46,36 @@ class AddEditFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_add_edit, container, false)
+        val updateBtn = view.findViewById<MaterialButton>(R.id.updateInventoryBtn)
+        val productName = view.findViewById<TextInputEditText>(R.id.productNameEditFrag)
+        val brandName = view.findViewById<TextInputEditText>(R.id.productBrandEditFrag)
+        val productParentCategory = view.findViewById<TextInputEditText>(R.id.productParentCatLayout)
+        val productSubCat = view.findViewById<TextInputEditText>(R.id.productCategoryEditFrag)
+        val productDescription = view.findViewById<TextInputEditText>(R.id.productDescriptionEditFrag)
+        val productPrice = view.findViewById<TextInputEditText>(R.id.productPriceEditFrag)
+        val productOffer = view.findViewById<TextInputEditText>(R.id.productOfferEditFrag)
+        val productQuantity = view.findViewById<TextInputEditText>(R.id.productQuantityEditFrag)
+        val productAvailableItems = view.findViewById<TextInputEditText>(R.id.productManufactureEditFrag)
+        val isVeg = view.findViewById<CheckBox>(R.id.productIsVegEditFrag)
+        val productManufactureDate = view.findViewById<TextInputEditText>(R.id.productAvailableItemsEditFrag)
+        val productExpiryDate = view.findViewById<TextInputEditText>(R.id.productExpiryEditFrag)
 
+        updateBtn.setOnClickListener {
+
+        }
         view.findViewById<ImageView>(R.id.addNewImage).setOnClickListener {
             imageHandler.showAlertDialog()
         }
         val imageLayout =view.findViewById<LinearLayout>(R.id.imageLayout)
         ImageHandler.gotImage.observe(viewLifecycleOwner){
-            count++
             val newView = LayoutInflater.from(context).inflate(R.layout.image_view,container,false)
             val image = newView.findViewById<ImageView>(R.id.productImage)
             image.setImageBitmap(it)
-
+            imageList.add(it)
             newView.findViewById<ImageButton>(R.id.deleteImage).setOnClickListener {
                 if(count>1){
                     imageLayout.removeView(newView)
+                    imageList.removeAt(count)
                     count--
                 }
                 else{
@@ -61,10 +83,23 @@ class AddEditFragment : Fragment() {
                 }
             }
             imageLayout.addView(newView,0)
+            count++
         }
         view.findViewById<MaterialToolbar>(R.id.materialToolbarEditProductFrag).setNavigationOnClickListener {
             parentFragmentManager.popBackStack()
         }
         return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        InitialFragment.hideSearchBar.value = true
+        InitialFragment.hideBottomNav.value = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        InitialFragment.hideSearchBar.value = false
+        InitialFragment.hideBottomNav.value = false
     }
 }
